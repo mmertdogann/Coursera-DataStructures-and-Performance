932f33cd01477b50989fea581ddfdcf34adcc896
mmertdogann <doganme@mef.edu.tr>
2019-11-21 21:46:06 +0300

    Week3 Done

--- MOOCTextEditor/src/document/Document.java
+++ MOOCTextEditor/src/document/Document.java
@@ -12,74 +12,71 @@ import java.util.regex.Pattern;
 public abstract class Document {
 
 	private String text;
-	
-	/** Create a new document from the given text.
-	 * Because this class is abstract, this is used only from subclasses.
+
+	/**
+	 * Create a new document from the given text. Because this class is abstract,
+	 * this is used only from subclasses.
+	 * 
 	 * @param text The text of the document.
 	 */
-	protected Document(String text)
-	{
+	protected Document(String text) {
 		this.text = text;
 	}
-	
-	/** Returns the tokens that match the regex pattern from the document 
-	 * text string.
-	 * @param pattern A regular expression string specifying the 
-	 *   token pattern desired
-	 * @return A List of tokens from the document text that match the regex 
-	 *   pattern
+
+	/**
+	 * Returns the tokens that match the regex pattern from the document text
+	 * string.
+	 * 
+	 * @param pattern A regular expression string specifying the token pattern
+	 *                desired
+	 * @return A List of tokens from the document text that match the regex pattern
 	 */
-	protected List<String> getTokens(String pattern)
-	{
+	protected List<String> getTokens(String pattern) {
 		ArrayList<String> tokens = new ArrayList<String>();
 		Pattern tokSplitter = Pattern.compile(pattern);
 		Matcher m = tokSplitter.matcher(text);
-		
+
 		while (m.find()) {
 			tokens.add(m.group());
 		}
-		
+
 		return tokens;
 	}
-	
+
 	// This is a helper function that returns the number of syllables
-	// in a word.  You should write this and use it in your 
+	// in a word. You should write this and use it in your
 	// BasicDocument class.
-	protected static int countSyllables(String word)
-	{
-	    //System.out.print("Counting syllables in " + word + "...");
+	protected static int countSyllables(String word) {
+		// System.out.print("Counting syllables in " + word + "...");
 		int numSyllables = 0;
 		boolean newSyllable = true;
 		String vowels = "aeiouy";
 		char[] cArray = word.toCharArray();
-		for (int i = 0; i < cArray.length; i++)
-		{
-		    if (i == cArray.length-1 && Character.toLowerCase(cArray[i]) == 'e' 
-		    		&& newSyllable && numSyllables > 0) {
-                numSyllables--;
-            }
-		    if (newSyllable && vowels.indexOf(Character.toLowerCase(cArray[i])) >= 0) {
+		for (int i = 0; i < cArray.length; i++) {
+			if (i == cArray.length - 1 && Character.toLowerCase(cArray[i]) == 'e' && newSyllable && numSyllables > 0) {
+				numSyllables--;
+			}
+			if (newSyllable && vowels.indexOf(Character.toLowerCase(cArray[i])) >= 0) {
 				newSyllable = false;
 				numSyllables++;
-			}
-			else if (vowels.indexOf(Character.toLowerCase(cArray[i])) < 0) {
+			} else if (vowels.indexOf(Character.toLowerCase(cArray[i])) < 0) {
 				newSyllable = true;
 			}
 		}
-		//System.out.println( "found " + numSyllables);
+		// System.out.println( "found " + numSyllables);
 		return numSyllables;
 	}
-	
-	/** A method for testing
+
+	/**
+	 * A method for testing
 	 * 
-	 * @param doc The Document object to test
+	 * @param doc       The Document object to test
 	 * @param syllables The expected number of syllables
-	 * @param words The expected number of words
+	 * @param words     The expected number of words
 	 * @param sentences The expected number of sentences
-	 * @return true if the test case passed.  False otherwise.
+	 * @return true if the test case passed. False otherwise.
 	 */
-	public static boolean testCase(Document doc, int syllables, int words, int sentences)
-	{
+	public static boolean testCase(Document doc, int syllables, int words, int sentences) {
 		System.out.println("Testing text: ");
 		System.out.print(doc.getText() + "\n....");
 		boolean passed = true;
@@ -87,55 +84,46 @@ public abstract class Document {
 		int wordsFound = doc.getNumWords();
 		int sentFound = doc.getNumSentences();
 		if (syllFound != syllables) {
-			System.out.println("\nIncorrect number of syllables.  Found " + syllFound 
-					+ ", expected " + syllables);
+			System.out.println("\nIncorrect number of syllables.  Found " + syllFound + ", expected " + syllables);
 			passed = false;
 		}
 		if (wordsFound != words) {
-			System.out.println("\nIncorrect number of words.  Found " + wordsFound 
-					+ ", expected " + words);
+			System.out.println("\nIncorrect number of words.  Found " + wordsFound + ", expected " + words);
 			passed = false;
 		}
 		if (sentFound != sentences) {
-			System.out.println("\nIncorrect number of sentences.  Found " + sentFound 
-					+ ", expected " + sentences);
+			System.out.println("\nIncorrect number of sentences.  Found " + sentFound + ", expected " + sentences);
 			passed = false;
 		}
-		
+
 		if (passed) {
 			System.out.println("passed.\n");
-		}
-		else {
+		} else {
 			System.out.println("FAILED.\n");
 		}
 		return passed;
 	}
-	
-	
+
 	/** Return the number of words in this document */
 	public abstract int getNumWords();
-	
+
 	/** Return the number of sentences in this document */
 	public abstract int getNumSentences();
-	
+
 	/** Return the number of syllables in this document */
 	public abstract int getNumSyllables();
-	
+
 	/** Return the entire text of this document */
-	public String getText()
-	{
+	public String getText() {
 		return this.text;
 	}
-	
+
 	/** return the Flesch readability score of this document */
-	public double getFleschScore()
-	{
-		double wordCount = (double)getNumWords();
-		return 206.835 - (1.015 * ((wordCount)/getNumSentences())) 
-				- (84.6 * (((double)getNumSyllables())/wordCount));
-	
+	public double getFleschScore() {
+		double wordCount = (double) getNumWords();
+		return 206.835 - (1.015 * ((wordCount) / getNumSentences()))
+				- (84.6 * (((double) getNumSyllables()) / wordCount));
+
 	}
-	
-	
-	
+
 }
